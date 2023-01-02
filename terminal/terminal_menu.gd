6ed88571f9a51c795594de86557afe1a0b983d41
Jory Leech <joryleech@gmail.com>
2023-01-01 17:04:34 -0800

    Text edit style log container

--- terminal/terminal_menu.gd
+++ terminal/terminal_menu.gd
@@ -2,16 +2,17 @@ extends Control
 
 
 # Called when the node enters the scene tree for the first time.
-var renderedLogs : Label
+var renderedLogs : TextEdit
 var textInput : TextEdit
 var scrollContainer : ScrollContainer
 func _ready():
 	Terminal.print_log.connect(_on_terminal_print)
 	Terminal.force_log.connect(_on_terminal_force_log)
 	textInput = get_node("TerminalInputContainer/TerminalInput")
-	renderedLogs = get_node("ScrollContainer/Logs/Label")
+	renderedLogs = get_node("ScrollContainer/Label")
 	scrollContainer = get_node("ScrollContainer")
 	scrollContainer.get_v_scroll_bar().changed.connect(_on_terminal_change_size)
+	
 
 func _on_terminal_change_size():
 	scrollContainer.scroll_vertical = scrollContainer.get_v_scroll_bar().max_value
